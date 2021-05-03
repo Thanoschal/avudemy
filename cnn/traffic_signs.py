5c93808620cce54581aa722b0b700108db1e975c
Athanasios Chalvatzaras <chalvatzaras@gmail.com>
2021-05-03 16:04:38 +0300

    data preprocessing

--- cnn/traffic_signs.py
+++ cnn/traffic_signs.py
@@ -1,3 +1,4 @@
+import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 import keras
@@ -7,12 +8,84 @@ from keras.optimizers import Adam
 from keras.utils.np_utils import to_categorical
 from keras.layers import Dropout, Flatten
 from keras.layers.convolutional import Conv2D, MaxPooling2D
+import pandas as pd
+import random
+import cv2
 np.random.seed(0)
 
 
 def start():
 
+    with open('../german-traffic-signs/train.p', 'rb') as f:
+        train_dat = pickle.load(f)
+    with open('../german-traffic-signs/valid.p', 'rb') as f:
+        val_dat = pickle.load(f)
+    with open('../german-traffic-signs/test.p', 'rb') as f:
+        test_dat = pickle.load(f)
 
+    X_train , y_train = train_dat['features'], train_dat['labels']
+    X_val, y_val = val_dat['features'], val_dat['labels']
+    X_test, y_test = test_dat['features'], test_dat['labels']
+
+    data = pd.read_csv('../german-traffic-signs/signnames.csv')
+
+    #grayscale the data, because in our case color is not supplying
+    #concrete information about the classification
+    def grayscale(img):
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        return img
+
+    #equalize the lighting and contrast of our images
+    def equalize(img):
+        cv2.equalizeHist(img)
+        return img
+
+    def preprocessing(img):
+        img = grayscale(img)
+        img = equalize(img)
+        img = img / 255
+        return img
+
+    #iterate through all images in X_train
+    #apply the preprocessing function
+    #convert the output of map to a list and then in numpy array
+    #store it back in X_train
+    X_train = np.array(list(map(preprocessing, X_train)))
+    X_val = np.array(list(map(preprocessing, X_val)))
+    X_test = np.array(list(map(preprocessing, X_test)))
+
+    #plt.imshow(X_train[random.randint(0, len(X_train - 1))], cmap='gray')
+    #plt.show()
+
+    #add a depth of 1 because the conv layer needs it
+    X_train = X_train.reshape(34799, 32, 32, 1)
+    X_val = X_val.reshape(X_val.shape[0], X_val.shape[1], X_val.shape[2], 1)
+    X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], X_test.shape[2], 1)
+
+    #one-hot-encode the labels of the sets
+    y_train = to_categorical(y_train, 43)
+    y_val = to_categorical(y_val, 43)
+    y_test = to_categorical(y_test, 43)
+
+
+    """
+    num_of_samples = []
+
+    cols = 5
+    num_classes = 43
+
+    fig, axs = plt.subplots(nrows=num_classes, ncols=cols, figsize=(5, 50))
+    fig.tight_layout()
+    for i in range(cols):
+        for j, row in data.iterrows():
+            x_selected = X_train[y_train == j]
+            axs[j][i].imshow(x_selected[random.randint(0, len(x_selected - 1)), :, :], cmap=plt.get_cmap("gray"))
+            axs[j][i].axis("off")
+            if i == 2:
+                axs[j][i].set_title(str(j) + "-" + row['SignName'])
+                num_of_samples.append(len(x_selected))
+    plt.show()
+   
     #define the LeNet model function
     def leNet_model():
         model = Sequential()
@@ -35,7 +108,7 @@ def start():
     model = leNet_model()
     print(model.summary())
     history = model.fit(X_train, y_train, epochs=10, validation_split=0.1, batch_size=400, verbose=1, shuffle=1)
-
+    """
 
 
 if __name__ == '__main__':
