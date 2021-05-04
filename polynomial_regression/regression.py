740e7d6d417c07f0e0609c059a3f8e34acb514e8
Athanasios Chalvatzaras <chalvatzaras@gmail.com>
2021-05-04 14:05:41 +0300

    polynomial regression with NN

--- polynomial_regression/regression.py
+++ polynomial_regression/regression.py
@@ -11,12 +11,19 @@ def start():
     Y = np.sin(X) + np.random.uniform(-0.5, 0.5, points)
 
     model = Sequential()
-    model.add(Dense(50, input_shape=1, activation='sigmoid'))
+    model.add(Dense(50, input_dim=1, activation='sigmoid'))
     model.add(Dense(30, activation='sigmoid'))
     model.add(Dense(1))
 
     adam = Adam(lr=0.01)
     model.compile(loss='mse', optimizer=adam)
+    model.fit(X, Y , epochs=50)
+
+    predictions = model.predict(X)
+    plt.scatter(X, Y)
+    plt.plot(X, predictions, 'ro')
+
+    plt.show()
 
 if __name__ == '__main__':
     start()
